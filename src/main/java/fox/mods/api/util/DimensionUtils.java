be7e1eece673ce347514453b03c4f74a51570b38
TheBlueEyeOfEnder <wilkinskyle18@outlook.com>
2024-09-10 15:27:42 -0400

    ### Changes - v1.4.0 Backport changes from 1.21.1 - TheBlueEyeOfEnder

--- src/main/java/fox/mods/api/util/DimensionUtils.java
+++ src/main/java/fox/mods/api/util/DimensionUtils.java
@@ -1,30 +1,26 @@
 package fox.mods.api.util;
 
 import fox.mods.api.nodimensions.configuration.NoDimensionsFileConfiguration;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DimensionUtils {
-    public static Pair<String, String> getDimensionNamespaceAndPath(ResourceKey<Level> dimension) {
-        if (dimension == null) {
+    public static Pair<String, String> getDimensionNamespaceAndPath(String dimension) {
+        if (dimension == null || dimension.isEmpty()) {
             return Pair.of("", "");
         }
-        String[] parts = dimension.toString().split(":");
-        if (parts.length == 2) {
-            return Pair.of(parts[0], parts[1]);
-        }
-        return Pair.of("", "");
+        String[] parts = dimension.split(":");
+        return Pair.of(parts[0], parts[1]);
     }
-
-    public static List<Pair<String, String>> getDisabledDimensions() {
-        return NoDimensionsFileConfiguration.getDimensionsDisabled().stream()
-                .map(dim -> dim.split(":"))
-                .filter(parts -> parts.length == 2)
-                .map(parts -> Pair.of(parts[0], parts[1]))
+    public static List<Pair<String, String>> getEnabledDimensions() {
+        List<? extends String> dimensions = NoDimensionsFileConfiguration.getDimensionsDisabled();
+        return dimensions.stream()
+                .map(dim -> {
+                    String[] parts = dim.split(":");
+                    return Pair.of(parts[0], parts[1]);
+                })
                 .collect(Collectors.toList());
     }
 }
